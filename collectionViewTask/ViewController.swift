278d726aa7d8cec4df3a494bcc1460f8b1e86741
Rabeeh KP <rabeeh@NFNs-MacBook-Air.local>
2017-11-20 14:58:13 +0530

    text and image showing in a collection view

--- collectionViewTask/ViewController.swift
+++ collectionViewTask/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UICollectionViewDelegate, UICollectionViewDataSource {
+    //MARK: -Outlets
+    
+    @IBOutlet weak var carCollectionView: UICollectionView!
+    
+    
+    //MARK: -Variables
+    
+    var cars = ["Audi","Bmw","Benz","Honda","RangeRover","Nissan","suzuki","porche"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: -Delegates
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cars.count
+    }
+ func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = carCollectionView.dequeueReusableCell(withReuseIdentifier: "newCell", for: indexPath) as! imgAndtxtCVcell
+    cell.collectionImg.image = UIImage(named:cars[indexPath.row])
+    cell.collectionLabel.text = cars[indexPath.row]
+        return cell
+    }
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
     }
 
 
-}
+
 
---
+++ collectionViewTask/imgAndtxtCVcell.swift
@@ -0,0 +1,18 @@
+//
+//  imgAndtxtCVcell.swift
+//  collectionViewTask
+//
+//  Created by Rabeeh KP on 20/11/17.
+//  Copyright © 2017 Rabeeh KP. All rights reserved.
+//
+
+import UIKit
+
+class imgAndtxtCVcell: UICollectionViewCell {
+    //MARK: -Outlets
+    
+    @IBOutlet weak var collectionLabel: UILabel!
+    @IBOutlet weak var collectionImg: UIImageView!
+    
+    
+}
